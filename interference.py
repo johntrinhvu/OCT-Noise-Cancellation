0170e1de5072c46bbf05880cf919058e73e5725b
John <johntrinhvu@yahoo.com>
2025-01-16 20:04:56 -0800

    add more data samples of cleaned images

--- interference.py
+++ interference.py
@@ -65,11 +65,11 @@ def postprocess_image(pred):
     return pred
 
 # Predict on a new image
-noisy_image_path = './dataset/input/102.bmp'
+noisy_image_path = './dataset/input/187.bmp'
 noisy_image = preprocess_image(noisy_image_path)
 cleaned_image = model.predict(noisy_image)
 
 # Save the cleaned image
 cleaned_image = postprocess_image(cleaned_image)
-cv2.imwrite('./cleaned_images/fourth_model_attempts/cleaned_image9.bmp', cleaned_image)
+cv2.imwrite('./cleaned_images/fourth_model_attempts/cleaned_image12.bmp', cleaned_image)
 print("Cleaned image saved!")
